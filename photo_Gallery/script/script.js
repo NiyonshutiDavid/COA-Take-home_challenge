6c6a2e6e769a3cbf2317afa09e2a3465f5811645
NiyonshutiDavid <144002340+NiyonshutiDavid@users.noreply.github.com>
2024-06-09 20:04:49 +0200

    Update script.js

--- photo_Gallery/script/script.js
+++ photo_Gallery/script/script.js
@@ -1,4 +1,13 @@
+/**
+ * This script adds a photo gallery to the webpage when the DOMContentLoaded event is fired.
+ * The gallery is populated with images from an array of objects, each containing the image source,
+ * title, and location.
+ */
 document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Array of objects representing the gallery images.
+     * @type {Array}
+     */
     const images = [
         {
             src: 'images/thumb1.jpg',
@@ -22,44 +31,58 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     ];
 
+    /**
+     * Function that creates the gallery by appending individual gallery items to the gallery element.
+     * Each gallery item contains an image, overlay, and text elements.
+     */
     function createGallery() {
         const gallery = document.querySelector('.gallery');
 
         images.forEach(image => {
+            // Create gallery item
             const item = document.createElement("div");
             item.className = "gallery-item";
 
+            // Create image element
             const img = document.createElement("img");
             img.src = image.src;
             img.alt = image.title;
 
+            // Create overlay element
             const overlay = document.createElement("div");
             overlay.className = "overlay";
 
+            // Create text element
             const text = document.createElement("div");
             text.className = "text";
 
+            // Create title element
             const title = document.createElement("h3");
             title.innerHTML = image.title;
 
+            // Create location element
             const location = document.createElement("p");
             location.textContent = image.location;
 
+            // Create link element
             const link = document.createElement("span");
             link.textContent = "Know more →";
             link.className = "text-link";
 
+            // Append child elements to gallery item
             text.appendChild(title);
             text.appendChild(location);
             item.appendChild(link);
-
             item.appendChild(text);
             item.appendChild(img);
             item.appendChild(overlay);
 
+            // Append gallery item to gallery
             gallery.appendChild(item);
         });
     }
 
+    // Call the createGallery function to populate the gallery with images in the index.html
     createGallery();
 });
+
